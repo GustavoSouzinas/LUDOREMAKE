3db50bdcae8f6c9be1f4df6086193e865e5193f3
LimasGuy <GustavoLimasGuy@gmail.com>
2025-09-09 12:36:30 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpacesComponent } from "./spaces/spaces.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SpacesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/spaces/spaces.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spaces',
+  imports: [CommonModule],
+  templateUrl: './spaces.component.html',
+  styleUrl: './spaces.component.css'
+})
+export class SpacesComponent implements OnInit {
+
+
+
+
+spaces: {id: number, row: number, col: number }[] = []
+
+ngOnInit(): void {
+ this.createSpaces()
+  
+  
+  
+;
+}
+
+createSpaces(){
+  this.spaces.push({ id: 1, row: 1, col: 1 });
+  this.spaces.push({ id: 2, row: 2, col: 1 });
+  this.spaces.push({ id: 3, row: 3, col: 1 });
+  this.spaces.push({ id: 4, row: 4, col: 1 });
+  this.spaces.push({ id: 5, row: 5, col: 1 });
+  this.spaces.push({ id: 6, row: 6, col: 1 });
+}
+
+}
