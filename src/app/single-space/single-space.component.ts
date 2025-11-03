dbab7d431b963dce79d3093db4e2dec53145d34b
LimasGuy <GustavoLimasGuy@gmail.com>
2025-11-03 13:46:16 -0300

    using 'grid-area' for board layout

--- src/app/single-space/single-space.component.ts
+++ src/app/single-space/single-space.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input} from '@angular/core';
 
+let counter = 0
 
 @Component({
   selector: 'app-single-space',
@@ -10,7 +11,15 @@ import { Component, Input} from '@angular/core';
 })
 export class SingleSpaceComponent {
 
+  number: number;
+
+  constructor(){
+    counter++;
+    this.number = counter;
+  }
+
   @Input() data: any;
   @Input() show_star = false;
   @Input() show_arrow = false;
+  
 }
